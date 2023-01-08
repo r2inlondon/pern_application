b7062999e4e2ac97a1afcf56180c007d9f16fac4
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-08 21:23:48 +0000

    feat : POST early stages working

--- index.js
+++ index.js
@@ -14,7 +14,34 @@ app.use(express.json()); // => allow us to access the req.body
 // create candidate
 app.post("/new", async (req, res) => {
   try {
-    res.json(req.body);
+    const {
+      firstName,
+      lastName,
+      gender,
+      email,
+      question1,
+      question2,
+      question3,
+    } = req.body;
+
+    console.log(gender);
+
+    const query = {
+      text: "INSERT INTO candidate (firstName, lastName, gender, email, question1, question2, question3) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      values: [
+        firstName,
+        lastName,
+        gender,
+        email,
+        question1,
+        question2,
+        question3,
+      ],
+    };
+
+    const newCandidate = await pool.query(query);
+
+    res.json(newCandidate.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
