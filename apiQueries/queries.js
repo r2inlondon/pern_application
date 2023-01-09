9b36298cb37b45208653312499f11f961917c702
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-09 11:53:14 +0000

    refactor : moved DB queries to separate folder, index.js looks neat now

---
+++ apiQueries/queries.js
@@ -0,0 +1,109 @@
+const pool = require("./db");
+
+const newCandidate = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      gender,
+      email,
+      question1,
+      question2,
+      question3,
+    } = req.body;
+
+    const query = {
+      text: "INSERT INTO candidate (firstName, lastName, gender, email, question1, question2, question3) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      values: [
+        firstName,
+        lastName,
+        gender,
+        email,
+        question1,
+        question2,
+        question3,
+      ],
+    };
+
+    const newCandidate = await pool.query(query);
+
+    res.json("Candidate was created !");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const getAllCandidates = async (req, res) => {
+  try {
+    const allCandidates = await pool.query("SELECT * FROM candidate;");
+    res.json(allCandidates.rows);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getOneCandidate = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const candidate = await pool.query(
+      "SELECT * FROM candidate WHERE c_id = $1;",
+      [id]
+    );
+    res.json(candidate.rows);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updateCandidate = async (req, res) => {
+  try {
+    const {
+      firstname,
+      lastname,
+      gender,
+      email,
+      question1,
+      question2,
+      question3,
+    } = req.body;
+
+    const { id } = req.params;
+
+    const query = {
+      text: "UPDATE candidate SET firstname = $1, lastname = $2, gender = $3, email = $4, question1 = $5, question2 = $6, question3 = $7 WHERE c_id = $8",
+      values: [
+        firstname,
+        lastname,
+        gender,
+        email,
+        question1,
+        question2,
+        question3,
+        id,
+      ],
+    };
+
+    const candidate = await pool.query(query);
+    res.json("Candidate has been UPDATED");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const deleteCandidate = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM candidate WHERE c_id = $1", [id]);
+    res.json("Candidate has been DELETED");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  newCandidate,
+  getAllCandidates,
+  getOneCandidate,
+  updateCandidate,
+  deleteCandidate,
+};
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+
+const {
+  newCandidate,
+  getAllCandidates,
+  getOneCandidate,
+  updateCandidate,
+  deleteCandidate,
+} = require("./apiQueries/queries");
 
 // middleware
 app.use(cors());
@@ -9,111 +16,20 @@ app.use(express.json()); // => allow us to access the req.body
 
 // ROUTES
 
+// create candidate
+app.post("/new", newCandidate);
+
 // Get all candidates
-app.get("/candidates", async (req, res) => {
-  try {
-    const allCandidates = await pool.query("SELECT * FROM candidate;");
-    res.json(allCandidates.rows);
-  } catch (err) {
-    console.error(err);
-  }
-});
+app.get("/candidates", getAllCandidates);
 
 // Get one candidate
-app.get("/candidates/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const candidate = await pool.query(
-      "SELECT * FROM candidate WHERE c_id = $1;",
-      [id]
-    );
-    res.json(candidate.rows);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-// create candidate
-app.post("/new", async (req, res) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      gender,
-      email,
-      question1,
-      question2,
-      question3,
-    } = req.body;
-
-    const query = {
-      text: "INSERT INTO candidate (firstName, lastName, gender, email, question1, question2, question3) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-      values: [
-        firstName,
-        lastName,
-        gender,
-        email,
-        question1,
-        question2,
-        question3,
-      ],
-    };
-
-    const newCandidate = await pool.query(query);
-
-    res.json("Candidate was created !");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+app.get("/candidates/:id", getOneCandidate);
 
 // Update candidate
-app.put("/candidates/:id", async (req, res) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      gender,
-      email,
-      question1,
-      question2,
-      question3,
-    } = req.body;
-
-    const { id } = req.params;
-
-    const query = {
-      text: "UPDATE candidate SET firstName = $1, lastName = $2, gender = $3, email = $4, question1 = $5, question2 = $6, question3 = $7 WHERE c_id = $8",
-      values: [
-        firstName,
-        lastName,
-        gender,
-        email,
-        question1,
-        question2,
-        question3,
-        id,
-      ],
-    };
-
-    const candidate = await pool.query(query);
-
-    res.json("Candidate has been UPDATED");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+app.put("/candidates/:id", updateCandidate);
 
 // Delete
-app.delete("/candidates/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    await pool.query("DELETE FROM candidate WHERE c_id = $1", [id]);
-    res.json("Candidate has been DELETED");
-  } catch (err) {
-    console.error(err);
-  }
-});
+app.delete("/candidates/:id", deleteCandidate);
 
 app.listen(5000, () => {
   console.log("server is starting on port 5000");
