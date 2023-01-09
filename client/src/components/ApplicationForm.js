5bbef5beab0d87b591c314b125ddd761b4e6a6d1
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-09 18:37:19 +0000

    feat : expandBackground function runs after form is selected

--- client/src/components/ApplicationForm.js
+++ client/src/components/ApplicationForm.js
@@ -1,6 +1,12 @@
 import PersonalDetailsForm from "./PersonalDeatailsForm";
+import { expandBackground } from "../js-functions/transparent-bg";
+import { useEffect } from "react";
 
 const ApplicationForm = () => {
+  useEffect(() => {
+    expandBackground();
+  }, []);
+
   return <PersonalDetailsForm />;
 };
 
