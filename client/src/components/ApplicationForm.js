7a12356c8adbb0d5dcd74fafe9d3594f5b2bb9d0
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-09 21:41:07 +0000

    feat : Survey component has been added

--- client/src/components/ApplicationForm.js
+++ client/src/components/ApplicationForm.js
@@ -1,13 +1,47 @@
-import PersonalDetailsForm from "./PersonalDeatailsForm";
+import { useEffect, Fragment, useState } from "react";
 import { expandBackground } from "../js-functions/transparent-bg";
-import { useEffect } from "react";
+import PersonalDetailsForm from "./PersonalDeatailsForm";
+import Survey from "./Survey";
 
 const ApplicationForm = () => {
+  const [showComponent, setShowComponent] = useState({
+    personalDetailsForm: true,
+    survey: false,
+  });
+
+  const [personalDetails, setPersonalDetails] = useState({
+    firstName: "",
+    lastName: "",
+    gender: "",
+    email: "",
+  });
+
+  const [questions, setQuestions] = useState({
+    question1: "",
+    question2: "",
+    question3: "",
+  });
+
   useEffect(() => {
     expandBackground();
   }, []);
 
-  return <PersonalDetailsForm />;
+  return (
+    <Fragment>
+      {showComponent.personalDetailsForm && (
+        <PersonalDetailsForm
+          setPersonalDetails={setPersonalDetails}
+          setShowComponent={setShowComponent}
+        />
+      )}
+      {showComponent.survey && (
+        <Survey
+          setQuestions={setQuestions}
+          setShowComponent={setShowComponent}
+        />
+      )}
+    </Fragment>
+  );
 };
 
 export default ApplicationForm;
---
+++ client/src/components/Survey.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+const Survey = ({ setQuestions, setShowComponent }) => {
+  const [music, setMusic] = useState("Pop");
+  const [pet, setPet] = useState("Cat person");
+  const [food, setFood] = useState("Yes");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setQuestions({
+      question1: music,
+      question2: pet,
+      question3: food,
+    });
+  };
+
+  return (
+    <div className="survey">
+      <form onSubmit={onSubmit}>
+        <div className="options-container">
+          <div className="question-container">
+            <div className="survey-txt">
+              <label>What kind of music do you prefer?</label>
+            </div>
+            <div className="select">
+              <select
+                id="select"
+                value={music}
+                onChange={(e) => setMusic(e.target.value)}
+              >
+                <option value="Linkedin">Metal</option>
+                <option value="Gumtree">Hip-Hop</option>
+                <option value="Newspaper">Pop</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="question-container">
+            <div className="survey-txt">
+              <label className="survey-txt">
+                Would you say that you are a:
+              </label>
+            </div>
+
+            <div className="select">
+              <select
+                id="select"
+                value={pet}
+                onChange={(e) => setPet(e.target.value)}
+              >
+                <option value="Dog person">Dog Person</option>
+                <option value="Cat person">Cat Person</option>
+              </select>
+            </div>
+          </div>
+          <div className="question-container">
+            <div className="survey-txt">
+              <label className="survey-txt">Are you a vegetarian ? </label>
+            </div>
+
+            <div className="select">
+              <select
+                id="select"
+                value={food}
+                onChange={(e) => setFood(e.target.value)}
+              >
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="next">
+          <input className="the-button" type="submit" value="SAVE"></input>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Survey;
