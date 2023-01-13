cc8d8b9f95e5cccd8e7b7eb3eb939758fff09184
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-13 22:08:09 +0000

    feat : antd Table added, showing all columns but more work is needed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { expandBackground } from "../../utils/expandBackground";
-import CandidatesTable from "./CandidatesTable";
+import { Table } from "antd";
+
 import { getCandidates } from "../../api/api-calls";
 
 const Dashboard = () => {
@@ -12,11 +13,49 @@ const Dashboard = () => {
     setCandidates(candidatesArray);
   };
 
+  const columns = [
+    {
+      title: "First Name",
+      dataIndex: "firstname",
+      key: "firstname",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastname",
+      key: "lastname",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Answer 1",
+      dataIndex: "question1",
+      key: "question1",
+    },
+    {
+      title: "Answer 2",
+      dataIndex: "question2",
+      key: "question2",
+    },
+    {
+      title: "Answer 3",
+      dataIndex: "question3",
+      key: "question3",
+    },
+  ];
+
   useEffect(() => {
     expandBackground("bg-bigger");
     getData();
   }, []);
-  return <CandidatesTable />;
+  return <Table dataSource={candidates} columns={columns} />;
 };
 
 export default Dashboard;
