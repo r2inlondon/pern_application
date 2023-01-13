3c51b7492b266b7ddaf38376b2202eca84c2d9e1
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-13 19:19:35 +0000

    refactor : refactor and organized files

---
+++ client/src/api/api-calls.js
@@ -0,0 +1,28 @@
+export const getCandidates = async () => {
+  let candidatesArray = [];
+  try {
+    const res = await fetch("http://localhost:5000/candidates");
+    candidatesArray = await res.json();
+  } catch (err) {
+    console.error(err);
+  }
+
+  return candidatesArray;
+};
+
+export const createCandidate = async (answers) => {
+  let newCandidate = "";
+
+  try {
+    newCandidate = await fetch("http://localhost:5000/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(answers),
+    });
+    console.log(newCandidate);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return newCandidate;
+};
---
+++ client/src/components/dashboard/CandidatesTable.js
@@ -0,0 +1,5 @@
+const CandidatesTable = (props) => {
+  return <h2>Candidates Table</h2>;
+};
+
+export default CandidatesTable;
