5ba17606bf8f81318873b0d99047b8613bf97cf8
Arturo Quiroz <r2inlondon@gmail.com>
2023-01-13 22:08:09 +0000

    feat : antd Table added, showing all columns but more work is needed

--- client/src/api/api-calls.js
+++ client/src/api/api-calls.js
@@ -12,14 +12,12 @@ export const getCandidates = async () => {
 
 export const createCandidate = async (answers) => {
   let newCandidate = "";
-
   try {
     newCandidate = await fetch("http://localhost:5000/new", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(answers),
     });
-    console.log(newCandidate);
   } catch (err) {
     console.error(err);
   }
