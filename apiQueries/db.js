58f75127124e10886032512db7fc22f72fbcd8e8
Arturo <r2inlondon@gmail.com>
2023-01-26 12:28:03 +0000

    feat : gitpod.yml updated to connect to dev db

--- apiQueries/db.js
+++ apiQueries/db.js
@@ -1,7 +1,9 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const devConfig = process.env.DATABASE_URL;
 
 const proConfig = process.env.DATABASE_URL;
 
